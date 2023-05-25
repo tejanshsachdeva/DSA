0041639bd4eaec0b1b360076cd98658f729074d7
Tejansh <89126093+tejanshsachdeva@users.noreply.github.com>
2023-05-25 13:04:25 +0530

    flood fill

--- Graphs/flood_fill.cpp
+++ Graphs/flood_fill.cpp
@@ -5,20 +5,20 @@ class Solution {
 private:
     void dfs(int row, int col, vector<vector<int>>&ans,
      vector<vector<int>>& image, int newColor, int delRow[], int delCol[],
-     int iniColor) {
+     int iniColor) {  //ini-color is the initial color
         // color with new color
-        ans[row][col] = newColor; 
+        ans[row][col] = newColor;  //elemment passed into dfs as marked
         int n = image.size();
         int m = image[0].size(); 
         // there are exactly 4 neighbours
-        for(int i = 0;i<4;i++) {
+        for(int i = 0;i<4;i++) {  //traversing the neighbours
             int nrow = row + delRow[i]; 
             int ncol = col + delCol[i]; 
             // check for valid coordinate 
             // then check for same initial color and unvisited pixel
-            if(nrow>=0 && nrow<n && ncol>=0 && ncol < m && 
+            if(nrow>=0 && nrow<n && ncol>=0 && ncol < m &&   
             image[nrow][ncol] == iniColor && ans[nrow][ncol] != newColor) {
-                dfs(nrow, ncol, ans, image, newColor, delRow, delCol, iniColor); 
+                dfs(nrow, ncol, ans, image, newColor, delRow, delCol, iniColor);   //executing dfs only when true
             }
         }
     }
@@ -26,8 +26,8 @@ public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, 
     int sr, int sc, int newColor) {
         // get initial color
-        int iniColor = image[sr][sc]; 
-        vector<vector<int>> ans = image; 
+        int iniColor = image[sr][sc];   //stores ini color
+        vector<vector<int>> ans = image;  //copy image to ans vector
         // delta row and delta column for neighbours
         int delRow[] = {-1, 0, +1, 0};
         int delCol[] = {0, +1, 0, -1}; 
