190d4621b943c2d82265b8b79e27d5adf232558c
Tejansh <89126093+tejanshsachdeva@users.noreply.github.com>
2023-06-10 17:06:57 +0530

    driver function added

--- Linked_List/add_ll.cpp
+++ Linked_List/add_ll.cpp
@@ -1,84 +1,99 @@
-class Solution
-{
-    private:
+#include <iostream>
+
+// Define the Node structure
+struct Node {
+    int data;
+    Node* next;
+    Node(int val) : data(val), next(nullptr) {}
+};
+
+class Solution {
+private:
     Node* reverse(Node* head) {
-        
         Node* curr = head;
-        Node* prev = NULL;
-        Node* next = NULL;
-        
-        while(curr != NULL) {
-            next = curr -> next;
-            curr -> next = prev;
+        Node* prev = nullptr;
+        Node* next = nullptr;
+
+        while (curr != nullptr) {
+            next = curr->next;
+            curr->next = prev;
             prev = curr;
             curr = next;
         }
         return prev;
     }
-    
-    void insertAtTail(struct Node* &head, struct Node* &tail, int val) {
-        
+
+    void insertAtTail(Node* &head, Node* &tail, int val) {
         Node* temp = new Node(val);
-        //empty list
-        if(head == NULL) {
+        if (head == nullptr) {
             head = temp;
             tail = temp;
-            return;
-        }
-        else
-        {
-            tail -> next = temp;
+        } else {
+            tail->next = temp;
             tail = temp;
         }
     }
-    
-    struct Node* add(struct Node* first, struct Node* second) {
+
+    Node* add(Node* first, Node* second) {
         int carry = 0;
-        
-        Node* ansHead = NULL;
-        Node* ansTail = NULL;
-        
-        while(first != NULL || second != NULL || carry != 0) {
-            
-            int val1 = 0;
-            if(first != NULL)
-                val1 = first -> data;
-                
-            int val2 = 0;
-            if(second !=NULL)
-                val2 = second -> data;
-            
-            
+
+        Node* ansHead = nullptr;
+        Node* ansTail = nullptr;
+
+        while (first != nullptr || second != nullptr || carry != 0) {
+            int val1 = (first != nullptr) ? first->data : 0;
+            int val2 = (second != nullptr) ? second->data : 0;
+
             int sum = carry + val1 + val2;
-            
-            int digit = sum%10;
-            
-            //create node and add in answer Linked List
+
+            int digit = sum % 10;
+
             insertAtTail(ansHead, ansTail, digit);
-            
-            carry = sum/10;
-            if(first != NULL)
-                first = first -> next;
-            
-            if(second != NULL)
-                second = second -> next;
+
+            carry = sum / 10;
+            if (first != nullptr)
+                first = first->next;
+            if (second != nullptr)
+                second = second->next;
         }
         return ansHead;
     }
-    public:
-    //Function to add two numbers represented by linked list.
-    struct Node* addTwoLists(struct Node* first, struct Node* second)
-    {
-        //step 1 -  reverse input LL
+
+public:
+    Node* addTwoLists(Node* first, Node* second) {
         first = reverse(first);
         second = reverse(second);
-        
-        //step2 - add 2 LL
+
         Node* ans = add(first, second);
-        
-        //step 3 
+
         ans = reverse(ans);
-        
+
         return ans;
     }
 };
+
+int main() {
+    // Test the Solution class
+    Solution solution;
+
+    // Create the first linked list: 3 -> 4 -> 2
+    Node* first = new Node(3);
+    first->next = new Node(4);
+    first->next->next = new Node(2);
+
+    // Create the second linked list: 4 -> 6 -> 5
+    Node* second = new Node(4);
+    second->next = new Node(6);
+    second->next->next = new Node(5);
+
+    // Add the two linked lists
+    Node* result = solution.addTwoLists(first, second);
+
+    // Print the result
+    while (result != nullptr) {
+        std::cout << result->data << " ";
+        result = result->next;
+    }
+
+    return 0;
+}
--- Linked_List/palindrome.cpp
+++ Linked_List/palindrome.cpp
@@ -1,62 +1,105 @@
-class Solution{
-  private:
-    Node* getMid(Node* head ) {
+#include <iostream>
+
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int value) {
+        data = value;
+        next = nullptr;
+    }
+};
+
+class Solution {
+private:
+    Node* getMid(Node* head) {
         Node* slow = head;
-        Node* fast = head -> next;
-        
-        while(fast != NULL && fast-> next != NULL) {
-            fast = fast -> next -> next;
-            slow = slow -> next;
+        Node* fast = head->next;
+
+        while (fast != nullptr && fast->next != nullptr) {
+            fast = fast->next->next;
+            slow = slow->next;
         }
-        
+
         return slow;
     }
+
     Node* reverse(Node* head) {
-        
         Node* curr = head;
-        Node* prev = NULL;
-        Node* next = NULL;
-        
-        while(curr != NULL) {
-            next = curr -> next;
-            curr -> next = prev;
+        Node* prev = nullptr;
+        Node* next = nullptr;
+
+        while (curr != nullptr) {
+            next = curr->next;
+            curr->next = prev;
             prev = curr;
             curr = next;
         }
+
         return prev;
     }
-  public:
-    //Function to check whether the list is palindrome.
-    bool isPalindrome(Node *head)
-    {
-        if(head -> next == NULL) {
+
+public:
+    // Function to check whether the list is palindrome.
+    bool isPalindrome(Node* head) {
+        if (head->next == nullptr) {
             return true;
         }
-        
-        //step 1 -> find Middle
+
+        // Step 1: Find Middle
         Node* middle = getMid(head);
-        //cout << "Middle " << middle->data << endl;
-        
-        //step2 -> reverse List after Middle
-        Node* temp = middle -> next;
-        middle -> next = reverse(temp);
-        
-        //step3 - Compare both halves
+
+        // Step 2: Reverse List after Middle
+        Node* temp = middle->next;
+        middle->next = reverse(temp);
+
+        // Step 3: Compare both halves
         Node* head1 = head;
-        Node* head2 = middle -> next;
-        
-        while(head2 != NULL) {
-            if(head2->data != head1->data) {
-                return 0;
+        Node* head2 = middle->next;
+
+        while (head2 != nullptr) {
+            if (head2->data != head1->data) {
+                return false;
             }
-            head1 = head1 -> next;
-            head2 = head2 -> next;
+            head1 = head1->next;
+            head2 = head2->next;
         }
-    
-        //step4 - repeat step 2
-        temp = middle -> next;
-        middle -> next = reverse(temp);
-        
+
+        // Step 4: Repeat Step 2
+        temp = middle->next;
+        middle->next = reverse(temp);
+
         return true;
     }
 };
+
+int main() {
+    // Example usage
+    Node* head = new Node(1);
+    head->next = new Node(2);
+    head->next->next = new Node(3);
+    head->next->next->next = new Node(2);
+    head->next->next->next->next = new Node(1);
+
+    Solution solution;
+    bool isPalindrome = solution.isPalindrome(head);
+
+    if (isPalindrome) {
+        cout << "The linked list is a palindrome." << endl;
+    } else {
+        cout << "The linked list is not a palindrome." << endl;
+    }
+
+    // Clean up memory
+    Node* current = head;
+    while (current != nullptr) {
+        Node* next = current->next;
+        delete current;
+        current = next;
+    }
+
+    return 0;
+}
