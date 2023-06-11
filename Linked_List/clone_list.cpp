292cd10e1018f2a8b51ce6494287fcac8c07e341
Tejansh <89126093+tejanshsachdeva@users.noreply.github.com>
2023-06-11 17:29:29 +0530

    clone random pointer list

---
+++ Linked_List/clone_list.cpp
@@ -0,0 +1,158 @@
+#include <iostream>
+using namespace std;
+
+// Node class
+class Node {
+public:
+    int data;
+    Node* next;
+    Node* arb;
+
+    Node(int d) {
+        data = d;
+        next = NULL;
+        arb = NULL;
+    }
+};
+class Solution
+{
+   private:
+    void insertAtTail(Node* &head, Node* &tail, int d) {
+        Node* newNode = new Node(d);
+        if(head == NULL) {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            tail -> next = newNode;
+            tail = newNode;
+        }
+    }
+    
+    void print(Node* head) {
+        while(head != NULL) {
+            cout << head -> data << " ";
+            head = head -> next;
+        }cout << endl;
+    }
+
+    void printRandom(Node* head) {
+        while(head != NULL) {
+            cout << " head data: " << head->data <<" ";
+            if(head ->arb != NULL) {
+                cout << " head random data" << head -> arb ->data;
+            }
+            else
+            {
+                cout << " head random data: NULL";
+            }
+            head = head -> next;
+            cout << endl;
+        }
+    }
+
+    public:
+    Node *copyList(Node *head)
+    {
+        //step 1: Create a Clone List
+        Node* cloneHead = NULL;
+        Node* cloneTail = NULL;
+        
+        Node* temp = head;
+        while(temp != NULL) {
+            insertAtTail(cloneHead, cloneTail, temp->data);
+            temp = temp -> next;
+        }
+        
+        // step 2: insert nodes of Clone List in between originalList
+        
+        Node* originalNode = head;
+        Node* cloneNode = cloneHead;
+        
+        while(originalNode != NULL && cloneNode != NULL) {
+            Node* next = originalNode -> next;
+            originalNode -> next = cloneNode;
+            originalNode = next;
+            
+            next = cloneNode -> next;
+            cloneNode -> next = originalNode;
+            cloneNode  = next;
+        }
+        
+        // step 3: Random pointer copy
+        originalNode = head;
+        cloneNode = cloneHead;
+        
+        while(originalNode != NULL && cloneNode != NULL) { 
+            
+            if(originalNode -> arb != NULL) {
+                cloneNode -> arb = originalNode -> arb -> next;
+            }
+            else
+            {
+                cloneNode -> arb  = NULL;
+            }
+            
+            cloneNode = cloneNode -> next;
+            originalNode = originalNode -> next;
+        }
+        
+        //step 4: revert step 2 changes
+        Node* original = head;
+        Node* copy = cloneHead;
+        
+         while (original && copy)
+            {
+                original->next =
+                 original->next? original->next->next : original->next;
+         
+                copy->next = copy->next?copy->next->next:copy->next;
+                original = original->next;
+                copy = copy->next;
+            }
+
+        // step 5 answer return
+        return cloneHead;
+    }
+};
+
+int main() {
+    // Create nodes
+    Node* node1 = new Node(1);
+    Node* node2 = new Node(2);
+    Node* node3 = new Node(3);
+    Node* node4 = new Node(4);
+    Node* node5 = new Node(5);
+
+    // Set arbitrary pointers
+    node1->arb = node3;
+    node2->arb = node1;
+    node3->arb = node5;
+    node4->arb = node2;
+    node5->arb = node4;
+
+    // Create linked list
+    node1->next = node2;
+    node2->next = node3;
+    node3->next = node4;
+    node4->next = node5;
+    node5->next = NULL;
+
+    // Create Solution object
+    Solution solution;
+
+    // Copy the linked list
+    Node* cloneHead = solution.copyList(node1);
+
+    // Print the original list
+    cout << "Original List: ";
+    solution.printRandom(node1);
+
+    // Print the cloned list
+    cout << "Cloned List: ";
+    solution.printRandom(cloneHead);
+
+    return 0;
+}
+
