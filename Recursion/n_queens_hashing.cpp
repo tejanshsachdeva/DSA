5aad1990904f22c99b0df79d51d970e891a9c820
Tejansh <89126093+tejanshsachdeva@users.noreply.github.com>
2023-06-16 23:12:11 +0530

    n_queens

--- Recursion/n_queens_hashing.cpp
+++ Recursion/n_queens_hashing.cpp
@@ -13,7 +13,7 @@ class Solution {
 
           board[row][col] = 'Q';   //marking the queen
           leftrow[row] = 1;  //mark the row, left diagonal and right diagonal as 1 in hash table
-          lowerDiagonal[row + col] = 1;
+          lowerDiagonal[row + col] = 1;  //row+col is the index of left diagonal, marked in hash table
           upperDiagonal[n - 1 + col - row] = 1;
           solve(col + 1, board, ans, leftrow, upperDiagonal, lowerDiagonal, n);  //if safe, then move to next column keeping same hashed value
           board[row][col] = '.';
@@ -32,7 +32,7 @@ class Solution {
       for (int i = 0; i < n; i++) {
         board[i] = s;
       }
-      vector < int > leftrow(n, 0), upperDiagonal(2 * n - 1, 0), lowerDiagonal(2 * n - 1, 0);
+      vector < int > leftrow(n, 0), upperDiagonal(2 * n - 1, 0), lowerDiagonal(2 * n - 1, 0);  //hash table for left diagonal, right diagonal and row of size 2*n-1
       solve(0, board, ans, leftrow, upperDiagonal, lowerDiagonal, n);
       return ans;
     }
