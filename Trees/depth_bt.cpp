c7eaf77fd583c36c8b76158b568996c34a851681
Tejansh <89126093+tejanshsachdeva@users.noreply.github.com>
2023-06-26 16:40:00 +0530

    tree ques

---
+++ Trees/depth_bt.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (root == NULL) return 0;
+
+        int lh = maxDepth(root->left);
+        int rh = maxDepth(root->right);
+
+        return 1 + max(lh, rh);
+    }
+};
+
+int main() {
+    // Create a sample binary tree
+    TreeNode* root = new TreeNode(3);
+    root->left = new TreeNode(9);
+    root->right = new TreeNode(20);
+    root->right->left = new TreeNode(15);
+    root->right->right = new TreeNode(7);
+
+    // Create an instance of the Solution class
+    Solution solution;
+
+    // Call the maxDepth function and print the result
+    int depth = solution.maxDepth(root);
+    cout << "Maximum depth of the binary tree: " << depth << endl;
+
+    // Deallocate memory
+    delete root->right->right;
+    delete root->right->left;
+    delete root->right;
+    delete root->left;
+    delete root;
+
+    return 0;
+}
